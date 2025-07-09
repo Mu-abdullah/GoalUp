6d64883e13d142f6a78dbb57a49b49e52c983987
Mu-abdullah <moabdullah588@gmail.com>
2025-07-09 18:29:19 +0300

    feat: Refactor coach home page to improve refresh functionality and UI structure

--- lib/features/coach/coach_home_page/presentation/cubits/home_academy_cubit/home_academy_cubit.dart
+++ lib/features/coach/coach_home_page/presentation/cubits/home_academy_cubit/home_academy_cubit.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../data/model/home_academy_model.dart';
 import '../../../data/repo/home_academy_repo.dart';
@@ -9,7 +9,7 @@ part 'home_academy_state.dart';
 class HomeAcademyCubit extends Cubit<HomeAcademyState> {
   HomeAcademyRepo repo;
   HomeAcademyCubit(this.repo) : super(CoachHomeInitial());
-
+  static HomeAcademyCubit get(context) => BlocProvider.of(context);
   Future<void> getAcademy(String id) async {
     emit(HomeAcademyLoading());
     var response = await repo.getAcademy(id);
--- lib/features/coach/coach_home_page/presentation/refactor/coach_home_page_body.dart
+++ lib/features/coach/coach_home_page/presentation/refactor/coach_home_page_body.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:hugeicons/hugeicons.dart';
 import 'package:sport/core/extextions/extentions.dart';
 
+import '../../../../../core/app/user/app_user_cubit/app_user_cubit.dart';
 import '../../../../../core/language/lang_keys.dart';
 import '../../../../../core/routes/routes_name.dart';
 import '../../../../../core/style/color/app_color.dart';
 import '../../../../../core/style/custom_widgets/home_count_card.dart';
 import '../../../../../core/style/statics/app_statics.dart';
+import '../cubits/home_academy_cubit/home_academy_cubit.dart';
+import '../cubits/player_counts_cubit/player_counts_cubit.dart';
 import '../widgets/academy_name_logo.dart';
 import '../widgets/academy_player_count.dart';
 
@@ -15,31 +18,45 @@ class CoachHomePageBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: AppPadding.symmetric(),
-      child: Column(
-        spacing: 20,
-        children: [
-          AcademyNameLogo(),
-          Row(
-            spacing: 10,
+    var academyId = AppUserCubit.get(context).academyId;
+    var count = PlayerCountsCubit.get(context);
+    var acdemy = HomeAcademyCubit.get(context);
+    return RefreshIndicator(
+      onRefresh: () async {
+        await Future.delayed(Duration(milliseconds: 500), () async {
+          await count.getPlayerCount(academyId);
+          await acdemy.getAcademy(academyId);
+        });
+      },
+      child: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        child: Padding(
+          padding: AppPadding.symmetric(),
+          child: Column(
+            spacing: 20,
             children: [
-              Expanded(child: AcademyPlayerCount()),
-              Expanded(
-                child: HomeCountCard(
-                  cardTitle: LangKeys.newPlayer,
-                  isTitle: true,
-                  cardIcon: HugeIcons.strokeRoundedAddTeam,
-                  isHighlighted: true,
-                  color: AppColors.black,
-                  onTap: () {
-                    context.pushNamed(RoutesNames.newPlayerNidScreen);
-                  },
-                ),
+              AcademyNameLogo(),
+              Row(
+                spacing: 10,
+                children: [
+                  Expanded(child: AcademyPlayerCount()),
+                  Expanded(
+                    child: HomeCountCard(
+                      cardTitle: LangKeys.newPlayer,
+                      isTitle: true,
+                      cardIcon: HugeIcons.strokeRoundedAddTeam,
+                      isHighlighted: true,
+                      color: AppColors.black,
+                      onTap: () {
+                        context.pushNamed(RoutesNames.newPlayerNidScreen);
+                      },
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
