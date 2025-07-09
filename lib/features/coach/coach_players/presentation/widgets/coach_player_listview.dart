1b48246837d3e12ede8bf548cb2e23915016274f
Mu-abdullah <moabdullah588@gmail.com>
2025-07-09 20:05:55 +0300

    feat: Conditionally display position filter based on player count in coach players list view

--- lib/features/coach/coach_players/presentation/widgets/coach_player_listview.dart
+++ lib/features/coach/coach_players/presentation/widgets/coach_player_listview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../data/model/get_players_model.dart';
+import '../cubits/coach_players_cubit/coach_players_cubit.dart';
 import 'coach_player_listview_item.dart';
 import 'position_filter.dart';
 
@@ -9,9 +10,10 @@ class CoachPlayersListView extends StatelessWidget {
   final List<GetPlayersModel> players;
   @override
   Widget build(BuildContext context) {
+    var coachPlayer = CoachPlayersCubit.get(context);
     return Column(
       children: [
-        PositionFilter(),
+        coachPlayer.players.length == 1 ? SizedBox.shrink() : PositionFilter(),
         ListView.builder(
           shrinkWrap: true,
           physics: NeverScrollableScrollPhysics(),
@@ -25,4 +27,3 @@ class CoachPlayersListView extends StatelessWidget {
     );
   }
 }
-
